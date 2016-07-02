028c738b7c4514396a5e8636f7bd5468468fb4eb
Denis Piazentin <denis@piazentin.com>
2016-07-02 11:06:20 -0300

    implementation/divisible_sum_pairs challenge

---
+++ algorithms/implementation/divisible_sum_pairs.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/divisible-sum-pairs
+
+
+def divisible_sum_pairs(n, k, a):
+  return len([True for j in range(n - 1, 0, -1) for i in range(j) if not (a[i] + a[j]) % k])
+
+
+n, k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+print(divisible_sum_pairs(n, k, a))
